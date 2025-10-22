181b01c4ede464ba3ae8208675fc8e6e72613994
Eternalizing <95091300+Eternalizing@users.noreply.github.com>
2025-10-22 02:26:13 -0400

    cambios

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@
 
 
 'use client'
-
+import './cuadromensaje.css'
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -113,29 +113,25 @@ export default function Hero() {
           aria-hidden="true"
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
         >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
-          />
+         
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-400">
-                <span aria-hidden="true" className="absolute inset-0" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
+          <div className=" sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm/6 text-black ring-1 ring-white/10 hover:ring-white/20">
+              — Mateo 19:6
+            
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-2xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
+            <div class="card">
+              <h1 className="bg text-2xl font-semibold tracking-tight text-balance text-[#3D5A40] sm:text-7xl py-1">
               “Así que ya no son dos, sino una sola carne;
 por tanto, lo que Dios unió, no lo separe el hombre.”
             </h1>
+            <div class="blob"></div>
+              </div>      
+            
+
             <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
               Leandro Padilla & Marli Rodríguez
             </p>
